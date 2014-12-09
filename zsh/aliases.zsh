dfe524cb47729ef3dd979a58530da1a0ae1e55bb
Nathan Lippi <nathan.lippi@gmail.com>
2014-12-09 13:03:51 -0500

    Add aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,22 @@
-alias reload!='. ~/.zshrc'
+alias ag="ag $* --pager 'less -R'"
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
+alias c='clear'
+alias t='touch'
+
+alias asdf='set-dvorak'
+alias aoeu='set-us'
+
+set-dvorak() {
+    setxkbmap dvorak
+    setxkbmap -option ctrl:swapcaps
+}
+
+set-us() {
+    setxkbmap us
+}
