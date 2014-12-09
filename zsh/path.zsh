168e97ffce0061912f00406bca1aa816a61b669e
Nathan Lippi <nathan.lippi@gmail.com>
2014-12-09 13:05:04 -0500

    Add path vars.

---
+++ zsh/path.zsh
@@ -0,0 +1,5 @@
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+# This is a path to a phpunit (and pear) executable
+export PATH=${PATH}:/home/nathan/pear/bin:/home/nathan/bin
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export JDK_HOME=/usr/lib/jvm/java-7-openjdk-amd64
