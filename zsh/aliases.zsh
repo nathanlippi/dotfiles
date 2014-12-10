9616bc4c5d9fbbb4bb5122269a3133fd99e94395
Nathan Lippi <nathan.lippi@gmail.com>
2014-12-10 14:31:57 -0500

    Add aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,3 @@
-alias ag="ag $* --pager 'less -R'"
-
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-
-alias c='clear'
-alias t='touch'
 alias sz='source ~/.zshrc'
 
 alias asdf='set-dvorak'
