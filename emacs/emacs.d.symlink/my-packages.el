a47b1269e2a6588bedddeb9bdcdf1f15334adaa7
Nathan Lippi <nathan.lippi@gmail.com>
2014-12-11 14:16:11 -0500

    Cleanup: Eliminate unnecessary comments, rename functions.

--- emacs/emacs.d.symlink/my-packages.el
+++ emacs/emacs.d.symlink/my-packages.el
@@ -7,40 +7,23 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
-(defvar required-packages
-  '(
-    anything
-    color-theme-sanityinc-tomorrow
-    edbi
-    flymake-css
-    flymake-cursor
-    flymake-jshint
-    flymake-php
-    js2-mode
-    key-chord
-    kill-ring-search
-    magit
-    pabbrev
-    php-mode
-    slime
-    yasnippet
-    web-beautify
-    zencoding-mode
-  ) "a list of packages to ensure are installed at launch.")
+(defvar required-packages '( anything
+  color-theme-sanityinc-tomorrow edbi flymake-css flymake-cursor
+  flymake-jshint flymake-php js2-mode key-chord kill-ring-search
+  magit pabbrev php-mode slime yasnippet web-beautify
+  zencoding-mode )
+  "a list of packages to ensure are installed at launch.")
 
-; method to check if all packages are installed
-(defun packages-installed-p ()
+(defun all-packages-installed-p ()
   (loop for p in required-packages
         when (not (package-installed-p p)) do (return nil)
         finally (return t)))
 
-; if not all packages are installed, check one by one and install the missing ones.
-(unless (packages-installed-p)
-  ; check for new packages (package versions)
-  (message "%s" "Emacs is now refreshing its package database...")
+(unless (all-packages-installed-p)
+  (message "%s" "Emacs is refreshing its packages...")
   (package-refresh-contents)
-  (message "%s" " done.")
-  ; install the missing packages
+  (message "%s" "Refreshing packages complete.")
+
   (dolist (p required-packages)
     (when (not (package-installed-p p))
       (package-install p))))
